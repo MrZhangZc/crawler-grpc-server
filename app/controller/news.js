e8bfd4e972e30e1b05d316afdf25e94598349b0a
zzc <1761997216@qq.com>
2021-08-25 12:09:28 +0800

    fix:shnbwwebdata

--- app/controller/news.js
+++ app/controller/news.js
@@ -26,8 +26,7 @@ module.exports = {
     await __pgQuery(sql, values)
     deleteFile(filename)
   },
-  GetNabNews: async (call, callback) => {
-    const { keyword } = call.request
+  GetHpNabNews: async (call, callback) => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage();
     await page.goto('https://nba.hupu.com/');
@@ -57,12 +56,52 @@ module.exports = {
       await __pgQuery(sql, values)
     }
   },
-  GetContent: async (call, callback) => {
-    // })
-    err = null
-    callback(err, { data: 'Hello' + call.request.keyword });
-    // update article set "screen_shot" = "screen_shot"||'{今晚吃西瓜}'
-    const res = await __pgQuery('update article set "screen_shot" = "screen_shot"||$1 where "id" = $2' , ['{晚上吃西瓜}', '712958ef-b25c-410e-969a-a69a7101b587'])
-    console.log(res)
-  }
+  GetShNabNews: async (call, callback) => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage();
+    await page.goto('https://sports.sohu.com/s/nba/');
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    })
+    const res = await page.$$eval('li.item > a', eles => eles.map(ele => ({
+      href: ele.href,
+      title: ele.innerText,
+      data_id: ele.href.slice(22, 38),
+      type: 'nba',
+      from: 'sh'
+    })))
+    await browser.close();
+    let err = null
+    callback(err, { data: 'success' });
+    for(let news of res){
+      const sql = 'INSERT INTO crawler("type", "title", "href", "data_id", "from") VALUES($1, $2, $3, $4, $5)'
+      const values = [news.type, news.title, news.href, news.data_id, news.from]
+      await __pgQuery(sql, values)
+    }
+  },
+  GetNbaWebNabNews: async (call, callback) => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage();
+    await page.goto('https://china.nba.com/news/');
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    })
+    const res = await page.$$eval('.news-wrap > a', eles => eles.map(ele => ({
+      href: ele.href,
+      title: ele.innerText,
+      // data_id: qs.parse(ele.href.split("?")[1]),
+      type: 'nba',
+      from: 'nba-web'
+    })))
+    await browser.close();
+    let err = null
+    callback(err, { data: 'success' });
+    // for(let news of res){
+    //   const sql = 'INSERT INTO crawler("type", "title", "href", "data_id", "from") VALUES($1, $2, $3, $4, $5)'
+    //   const values = [news.type, news.title, news.href, news.data_id, news.from]
+    //   await __pgQuery(sql, values)
+    // }
+  },
 }
